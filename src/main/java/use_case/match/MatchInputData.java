33492888b521a587407cf3f43fa948c9075a5bdd
luiscc02 <luis.calderon@mail.utoronto.ca>
2024-11-25 21:41:11 -0500

    Start of Like Use CaSE

---
+++ src/main/java/use_case/match/MatchInputData.java
@@ -0,0 +1,8 @@
+package use_case.match;
+
+import entity.User;
+
+public class MatchInputData {
+    private User user1;
+    private User user2;
+}
---
+++ src/main/java/use_case/match/MatchOutputData.java
@@ -0,0 +1,6 @@
+package use_case.match;
+
+import java.util.ArrayList;
+
+public class MatchOutputData {
+}
